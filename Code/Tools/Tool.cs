8c196f43c460d12ce26a3758b545bdad367c4dc8
catapillie <espritox@gmail.com>
2022-12-26 01:00:48 +0100

    add tools and tool manager

---
+++ Code/Tools/Tool.cs
@@ -0,0 +1,49 @@
+﻿namespace Celeste.Mod.CommunalTools.Tools;
+
+/// <summary>
+/// Represents a base tool, which can execute code before and after a <see cref="Level"/>'s update and render calls.
+/// </summary>
+public abstract class Tool
+{
+    /// <summary>
+    /// The reference to the current level.
+    /// </summary>
+    public Level Level { get; internal set; }
+
+    /// <summary>
+    /// Called after the <see cref="Level.Level"/> constructor.
+    /// </summary>
+    public virtual void Begin() { }
+
+    /// <summary>
+    /// Called when this tool is registered.
+    /// </summary>
+    public virtual void Registered() { }
+
+    /// <summary>
+    /// Called when this tool is unregistered.
+    /// </summary>
+    public virtual void Unregistered() { }
+
+    /// <summary>
+    /// Called before <see cref="Level.Update"/>.
+    /// </summary>
+    /// <returns>A <see cref="bool"/> that determines whether the current <see cref="Level"/> itself should be updated, or skipped when <c>false</c> is returned.</returns>
+    public virtual bool UpdateBefore() => true;
+
+    /// <summary>
+    /// Called after <see cref="Level.Update"/>.
+    /// </summary>
+    public virtual void UpdateAfter() { }
+
+    /// <summary>
+    /// Called before <see cref="Level.Render"/>.
+    /// </summary>
+    /// <returns>A <see cref="bool"/> that determines whether the current <see cref="Level"/> itself should be rendered, or skipped when <c>false</c> is returned.</returns>
+    public virtual bool RenderBefore() => true;
+
+    /// <summary>
+    /// Called after <see cref="Level.Render"/>.
+    /// </summary>
+    public virtual void RenderAfter() { }
+}
---
+++ Code/Tools/ToolManager.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Celeste.Mod.CommunalTools.Tools;
+
+/// <summary>
+/// Handler class for all <see cref="Tool"/>s.
+/// </summary>
+public static class ToolManager
+{
+    private static readonly Dictionary<Type, Tool> tools = new();
+
+    public static void Register<T>()
+        where T : Tool, new()
+    {
+        if (Has<T>())
+            throw new InvalidOperationException($"Tool of type {typeof(T)} is already registered!");
+        
+        T tool = new();
+        tools.Add(typeof(T), tool);
+
+        tool.Registered();
+    }
+
+    public static void Unregister<T>()
+        where T : Tool
+    {
+        if (!Has<T>())
+            throw new InvalidOperationException($"Tool of type {typeof(T)} was never registered!");
+
+        T tool = tools[typeof(T)] as T;
+        tools.Remove(typeof(T));
+
+        tool.Unregistered();
+    }
+
+    public static void UnregisterAll()
+    {
+        tools.Clear();
+    }
+
+    public static bool Has<T>() where T : Tool
+        => tools.ContainsKey(typeof(T));
+
+    public static T Get<T>()
+        where T : Tool
+    {
+        if (!Has<T>())
+            throw new InvalidOperationException($"Tool of type {typeof(T)} was never registered!");
+
+        return tools[typeof(T)] as T;
+    }
+
+    internal static void Load()
+    {
+        On.Celeste.Level.ctor += Mod_Level_ctor;
+        On.Celeste.Level.Update += Mod_Level_Update;
+        On.Celeste.Level.Render += Mod_Level_Render;
+    }
+
+    internal static void Unload()
+    {
+        On.Celeste.Level.ctor -= Mod_Level_ctor;
+        On.Celeste.Level.Update -= Mod_Level_Update;
+        On.Celeste.Level.Render -= Mod_Level_Render;
+    }
+
+    private static void Mod_Level_ctor(On.Celeste.Level.orig_ctor orig, Level self)
+    {
+        orig(self);
+
+        foreach (Tool tool in tools.Values)
+        {
+            tool.Level = self;
+            tool.Begin();
+        }
+    }
+
+    private static void Mod_Level_Update(On.Celeste.Level.orig_Update orig, Level self)
+    {
+        bool update = true;
+        foreach (Tool tool in tools.Values)
+            update &= tool.UpdateBefore();
+
+        if (update)
+            orig(self);
+
+        foreach (Tool tool in tools.Values)
+            tool.UpdateAfter();
+    }
+
+    private static void Mod_Level_Render(On.Celeste.Level.orig_Render orig, Level self)
+    {
+        bool render = true;
+        foreach (Tool tool in tools.Values)
+            render &= tool.RenderBefore();
+
+        if (render)
+            orig(self);
+
+        foreach (Tool tool in tools.Values)
+            tool.RenderAfter();
+    }
+}
