14bf6cf1089713e3cd64476bc8b5ea8d4587aa39
catapillie <espritox@gmail.com>
2022-12-31 00:03:24 +0100

    rename and rebuild audio bank

--- Code/ModSFX.cs
+++ Code/ModSFX.cs
@@ -2,15 +2,15 @@
 
 public static class ModSFX
 {
-    public const string sfx_screenshot_success = "event:/communal_tools/sfx/screenshot/success";
-    public const string sfx_screenshot_selection = "event:/communal_tools/sfx/screenshot/selection";
-    public const string sfx_screenshot_fix_selection = "event:/communal_tools/sfx/screenshot/fix_selection";
+    public const string sfx_screenshot_success = "event:/modder_toolkit/sfx/screenshot/success";
+    public const string sfx_screenshot_selection = "event:/modder_toolkit/sfx/screenshot/selection";
+    public const string sfx_screenshot_fix_selection = "event:/modder_toolkit/sfx/screenshot/fix_selection";
 
-    public const string sfx_recording_countdown_three = "event:/communal_tools/sfx/recording/countdown_three";
-    public const string sfx_recording_countdown_two = "event:/communal_tools/sfx/recording/countdown_two";
-    public const string sfx_recording_countdown_one = "event:/communal_tools/sfx/recording/countdown_one";
-    public const string sfx_recording_go = "event:/communal_tools/sfx/recording/go";
-    public const string sfx_recording_success = "event:/communal_tools/sfx/recording/success";
+    public const string sfx_recording_countdown_three = "event:/modder_toolkit/sfx/recording/countdown_three";
+    public const string sfx_recording_countdown_two = "event:/modder_toolkit/sfx/recording/countdown_two";
+    public const string sfx_recording_countdown_one = "event:/modder_toolkit/sfx/recording/countdown_one";
+    public const string sfx_recording_go = "event:/modder_toolkit/sfx/recording/go";
+    public const string sfx_recording_success = "event:/modder_toolkit/sfx/recording/success";
 
-    public const string BANK = "bank:/communal_tools";
+    public const string BANK = "bank:/modder_toolkit";
 }
